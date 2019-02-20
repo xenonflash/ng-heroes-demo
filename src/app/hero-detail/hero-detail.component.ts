b12baac0716366b316b91cf6b62001b4b4909782
yankechao <yankechao@vipkid.com.cn>
2019-02-20 20:38:42 +0800

    完成 hero-detail 组件逻辑

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero'
 
 @Component({
-  selector: 'app-hero-detail',
+  selector: 'hero-detail', // 组件名 在父组件中 标签名
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.styl']
 })
 export class HeroDetailComponent implements OnInit {
-
+  @Input() hero: Hero // Input 相当于 props 声明
   constructor() { }
 
   ngOnInit() {
