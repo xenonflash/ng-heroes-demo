31e01b8e177ab7d328ba192373ecfed0770663a5
yankechao <yankechao@vipkid.com.cn>
2019-02-21 15:51:24 +0800

    添加 message 相关组件和 服务

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { HEROS } from './mock/heros'
 import { Hero } from './hero'
 import { Observable, of } from 'rxjs'
+import { MessageService } from './message.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
   getHeros(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heros')
     return of(HEROS)
   }
 }
